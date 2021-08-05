dfdb085a02d01498226d1e68dbe5b345815c20b9
glebhubarevich <g.hubarevich@gmail.com>
2021-08-05 16:23:19 +0200

    fix slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,23 +4,28 @@ const sliderContainer = document.querySelector('.slider');
 const sliderBody = document.querySelector('.slider__container');
 const slides = document.querySelectorAll('.slider__slide');
 const slide = document.querySelector('.slider__slide_img');
-
-//Calculate how many slides fit on screen and get the max number of slider windows
-const slideWidth = slide.offsetWidth + 30; //30px=margin/column-gap
-const slidesOnScreen = Math.trunc(window.innerWidth / slideWidth);
-const maxSlide = slides.length - slidesOnScreen;
-
 let curSlide = 0;
 
-const goToSlide = (s) => (sliderBody.style.transform = `translateX(${-s * slideWidth}px)`);
+// //Calculate how many slides fit on screen and get the max number of slider windows
+// const slideWidth = slide.offsetWidth + 30; //30px=margin/column-gap
+// const slidesOnScreen = Math.trunc(window.innerWidth / slideWidth);
+// let maxSlide = slides.length - slidesOnScreen;
+
+const getSlideWidth = () => slide.offsetWidth + 30; //30px = left margin/column-gap
+const getMaxSlide = () => {
+	const slidesOnScreen = Math.trunc(window.innerWidth / getSlideWidth());
+	return slides.length - slidesOnScreen;
+};
+
+const goToSlide = (s) => (sliderBody.style.transform = `translateX(${-s * getSlideWidth()}px)`);
 
 const prewSlide = () => {
-	if (curSlide === 0) curSlide = maxSlide;
+	if (curSlide === 0) curSlide = getMaxSlide();
 	else curSlide--;
 	goToSlide(curSlide);
 };
 const nextSlide = () => {
-	if (curSlide === maxSlide) curSlide = 0;
+	if (curSlide === getMaxSlide()) curSlide = 0;
 	else curSlide++;
 	goToSlide(curSlide);
 };
