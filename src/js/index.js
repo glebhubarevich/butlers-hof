b7ddf1a8ddb91039ac22c5055780e14677cfd60e
glebhubarevich <g.hubarevich@gmail.com>
2021-08-06 20:12:14 +0200

    bug fixes

--- src/js/index.js
+++ src/js/index.js
@@ -19,4 +19,30 @@ $(document).ready(function () {
 		$('.modal').fadeIn();
 		$('body').css('overflowY', 'hidden');
 	});
+
+	//Toggle nav menu:
+	$('.navigation__button').on('click', () => {
+		//
+		if ($('.contact__map').css('display') === 'block') {
+			$('.contact__map').fadeOut();
+		}
+		//
+		//
+		else if ($('.contact__map').css('display') === 'none') {
+			$('.contact__map').fadeIn();
+		}
+
+		if ($('.navigation__content').hasClass('navigation-active') === true) {
+			$('.navigation').fadeOut();
+			$('body').css('overflowY', 'scroll');
+		}
+		//
+		else if ($('.navigation__content').hasClass('navigation-active') === false) {
+			$('.navigation').fadeIn();
+			$('body').css('overflowY', 'hidden');
+		}
+
+		$('.navigation__content').toggleClass('navigation-active');
+		$('.navigation__button>span').toggleClass('nav-btn-active');
+	});
 });
